f654ed6235392c501a15d2fb3e0cb61c36b71102
Francisco <Francisco@DESKTOP-F8ACFSC>
2019-02-21 15:31:13 -0300

    Lista de exercicios

---
+++ exercicio2.php
@@ -0,0 +1,11 @@
+<?php
+    $vetor=array();
+    for ($i=1;$i<=50;$i++){
+        $vetor[$i]=($i-1);
+        echo "Vetor ".$vetor[$i]." = ".$i."<br>";
+    }
+?>
+    
+
+
+
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+    $vetor=array();
+    for ($i=0;$i<=50;$i++){
+        $vetor[$i]=42;
+        echo "Vetor ".$i." = ".$vetor[$i]."<br>";
+    }
+?>
