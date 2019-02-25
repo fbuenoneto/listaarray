f187cbc3099ed67597f0c0e25160019236096cd5
Francisco <Francisco@DESKTOP-F8ACFSC>
2019-02-25 09:38:37 -0300

    Lista de exercicios 2, exercicio 4

---
+++ exercicio4.php
@@ -0,0 +1,11 @@
+<?php
+    for ($i=0;$i<=50;$i++){
+        $n=$i*2;
+        if ($i<10){
+            echo "Vetor [0".$i."] = ".$n."<br>";
+        }else{
+            echo "Vetor [".$i."] = ".$n."<br>";
+        }
+    }
+?>
+
